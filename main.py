63a2d2066fd8d642db4c49e1bbd77b4bdeae73b3
Gokay <rexpo2433@gmail.com>
2022-11-11 19:50:25 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 import os
+import psycopg2
 
 Base = declarative_base()
 app = Flask(__name__)
